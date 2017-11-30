8d09eb1e1b63a6a10c80b9c0823e9a1b0d601320
seanpdonovan <seandonbu@gmail.com>
2017-11-29 22:45:38 -0600

    Update client.py

--- Pi Files/client.py
+++ Pi Files/client.py
@@ -5,9 +5,10 @@ from subprocess import call
 from socket import *
 import time
 
-#set host to the IP of the server computer
-
+# set the default ip as what my computer is
 host = "192.168.1.113"
+
+# can pass another server ip when calling this program
 if len(sys.argv) == 2:
     host = sys.argv[1]
 
@@ -15,13 +16,21 @@ port = 13000
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 
+# contains list of authorized users
 USER_FILE = "users.txt"
+
+# keeps the local copy of the output log
 OUTPUT_FILE = "output.txt"
 
+# time the lock stays unlocked before relocking
+RELOCK_DELAY = 10
+
+# UH ID cards are "%1234567;"
 def parseInput(InString):
     newString = InString[1:8] + " " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return newString
 
+# check if authorized
 def isApproved(User):
     approved = False
     if User in open(USER_FILE).read():
@@ -32,23 +41,23 @@ Input = ""
 while Input != "exit":
     Input = raw_input()
     if Input != "":
-        if Input == "exit":
+        if Input == "exit":    # type exit to close client and server
             UDPSock.sendto(Input, addr)
             UDPSock.close()
             exit()
-        Input = parseInput(Input)
-        if isApproved(Input[:7]):
-            UDPSock.sendto(Input, addr)
-            out = open(OUTPUT_FILE, 'a')
+        Input = parseInput(Input)    # convert to usable string from card add date/time
+        if isApproved(Input[:7]):    # check user ID
+            UDPSock.sendto(Input, addr)    # send ID and date/time to server
+            out = open(OUTPUT_FILE, 'a')    # write to local log
             out.write(Input + '\n')
             out.close()
-            call(["sh", "unlock.sh"])
-            time.sleep(10)
-            call(["sh", "lock.sh"])
+            call(["sh", "unlock.sh"])    # unlock the lock
+            time.sleep(RELOCK_DELAY)
+            call(["sh", "lock.sh"])    # relock the lock
         elif isApproved(Input[:7]) == False:
             AD = "Access Denied for: " + Input
-            UDPSock.sendto(AD, addr)
-            out = open(OUTPUT_FILE, 'a')
+            UDPSock.sendto(AD, addr)    # send Access Denied message to server
+            out = open(OUTPUT_FILE, 'a')    # write Access Denied message to local log
             out.write(AD + '\n')
             out.close()
     print(Input)
